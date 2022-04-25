6c3c0b282a80887b1a4f489ae166b5f5cf640702
Sudarshan Rai <rrraishati@gmail.com>
2022-04-25 12:51:53 +0545

    feat: icon dispatcher added

--- dist/res/index.d.ts
+++ dist/res/index.d.ts
@@ -1,9 +1,9 @@
 declare const icons: {
-    textAnnotate: string;
-    censorAnnotate: string;
-    markAnnotate: string;
-    right: string;
-    close: string;
+  textAnnotate: string;
+  censorAnnotate: string;
+  markAnnotate: string;
+  right: string;
+  close: string;
 };
 declare const getIcon: (name: keyof typeof icons) => string;
 export { getIcon };
--- dist/types/IModes/ICanvas.d.ts
+++ dist/types/IModes/ICanvas.d.ts
@@ -1,16 +1,16 @@
 declare type appId = string | number;
 export declare namespace ICanvasMode {
-    type IMarkMode = 'mark' | 'censored' | 'text';
-    interface IConfig {
-        button?: boolean;
-        buttonLabel?: string;
-        initialMarkMode?: IMarkMode;
-        appId: appId;
-        enableForm?: boolean;
-        buttonPosition?: 'left' | 'bottom';
-    }
+  type IMarkMode = 'mark' | 'censored' | 'text';
+  interface IConfig {
+    button?: boolean;
+    buttonLabel?: string;
+    initialMarkMode?: IMarkMode;
+    appId: appId;
+    enableForm?: boolean;
+    buttonPosition?: 'left' | 'bottom';
+  }
 }
 export interface feedbackData {
-    appId: appId;
+  appId: appId;
 }
 export {};
